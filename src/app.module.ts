cd792e7e3470888925cdadeefc975e6cfeda2462
Luca Nardelli <lunard@gmail.com>
2022-05-07 19:36:36 +0200

    Added mongoose module and connection with the MongoDB Atlas database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { SensorsModule } from './sensors/sensors.module';
 
 @Module({
-  imports: [SensorsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@air4me.frvrk.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`),
+    SensorsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
